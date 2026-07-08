16cdc052ba82c079a5dc296835b1b8891ecabe69
moreno0308 <WELLINGTONMORENO03081996@GMAIL.COM>
2026-07-08 13:50:05 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Depois de implantar o Apps Script como "App da Web", cole a URL aqui
 // (termina em /exec). É a única coisa que precisa editar neste arquivo.
-const APPS_SCRIPT_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzEnlODuAe95lFw0lFHqd0UvUAWur54Fb8GlBs-A_3jroey3Vh5NY3opOYxDchQKc7VtQ/exec";
